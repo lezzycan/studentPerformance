078307b0116abfc45a1d85e2713441f4e99f6a76
Lezzycan <waheedolalekan23@gmail.com>
2026-02-18 04:14:55 +0000

    packages added

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)-> List[str]:
+    '''
+    Docthis function will return the list of requirements
+    string for get_requirements
+    
+    :param file_path: Description
+    :type file_path: str
+    :return: Description
+    :rtype: list[str]
+    '''
+    requirements = []
+    with open(file_path) as f:
+        requirements = [req.replace("\n", "") for req in f.readlines()]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Olalekan Waheed',
+author_email='waheedolalekan23@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt'),
+
+)
+
